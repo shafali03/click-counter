1b205fbf7c605312f1a7f16be4d7823126a7ff06
shafali03 <shaf03sm@gmail.com>
2021-03-30 19:54:44 +0100

    Displays error message when count number is below 0

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 
 function App() {
   const [count, setCount] = useState(0)
+  const [error, setError] = useState(false)
 
   return (
     <div data-test="component-app">
@@ -10,12 +11,27 @@ function App() {
       The counter is currently&nbsp;
         <span data-test='count'>{count}</span>
       </h1>
-      <button data-test="increment-button"
-      onClick={() => setCount(count + 1)}>Increment counter
-      </button>
+      <div data-test='error-message' className={`error ${error ? '' : 'hidden'}`}>
+        The counter cannot go below 0
+      </div>
 
+      <button data-test="increment-button"
+      onClick={() =>  {
+        if (error) { setError(false) }
+          setCount(count + 1)
+        }
+      }>
+        Increment counter
+      </button>
       <button data-test="decrement-button"
-      onClick={() => setCount(count -1)}>
+      onClick={() => {
+        if (count > 0){
+          setCount(count -1)
+        } else {
+          setError(true)
+        }
+      } 
+    }>
         Decrement counter
       </button>
     </div>
